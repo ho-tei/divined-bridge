ecb172db3847f269fba11cf8fd5a4887a9dce83e
ho-tei <117127416+ho-tei@users.noreply.github.com>
2025-03-17 03:22:59 +0100

    rip my clean source control

--- src/discord/other/updateUsers.js
+++ src/discord/other/updateUsers.js
@@ -3,7 +3,6 @@ const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
 const config = require("../../../config.json");
 const Logger = require("../../Logger.js");
 const cron = require("node-cron");
-const guild = hypixel.getGuild("name", "Divined"); //! hard coded guild name but its whatever
 const { readFileSync } = require("fs");
 
 if (config.verification.autoUpdater) {
@@ -25,29 +24,29 @@ if (config.verification.autoUpdater) {
   }
 
   try {
-    const guildMembers = guild.members();
-
-    guildMembers.array.forEach(async (member) => {
-      if (((Date.now() / 1000) - member.joinedAtTimestamp) > 604800) {
-        console.log(">1week");
-        const discordID = Object.keys(linked).find((key) => linked[key] === member.uuid);
-        if (discordID === undefined) {
-          return console.log(`${member} is not linked`);
+    hypixel.getGuild("name", "Divined").then((guild) => {
+      guild.members.forEach(async (member) => {
+        if (((Date.now() / 1000) - member.joinedAtTimestamp) > 604800) {
+          console.log(">1week");
+          const discordID = Object.keys(linked).find((key) => linked[key] === member.uuid);
+          if (discordID === undefined) {
+            return console.log(`${member} is not linked`);
+          }
+  
+          // asign role
+          try {
+            const guild = await global.client.guilds.fetch("1204303173150449696"); //! hard coded guild id!
+            const member = await guild.members.fetch(discordID);
+            await member.roles.add('1345409650719457312');
+            console.log(`Role assigned to ${member.user.tag}`);
+          } catch (error) {
+            console.error("Error assigning role:", error);
+          }
+  
+        } else {
+          config.log(`${member} has been in the guild for less than a week`);
         }
-
-        // asign role
-        try {
-          const guild = await global.client.guilds.fetch("1204303173150449696"); //! hard coded guild id!
-          const member = await guild.members.fetch(discordID);
-          await member.roles.add('1345409650719457312');
-          console.log(`Role assigned to ${member.user.tag}`);
-        } catch (error) {
-          console.error("Error assigning role:", error);
-        }
-
-      } else {
-        config.log(`${member} has been in the guild for less than a week`);
-      }
+      });
     });
 
   } catch (e) {
