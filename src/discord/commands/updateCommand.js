64e4d27308c9198c0eb5b30c4e4774d6e43ee319
Exo <jorgeperezg24@hotmail.com>
2025-03-24 13:09:26 -0400

    Update updateCommand.js

--- src/discord/commands/updateCommand.js
+++ src/discord/commands/updateCommand.js
@@ -1,70 +1,56 @@
-module.exports = {
-  name: "update",
-  verificationCommand: true,
-  description: "Update your current roles",
-
-  execute: async (interaction, user) => {
+execute: async (interaction, user) => {
     try {
-      const linkedData = readFileSync("data/linked.json");
-      if (!linkedData) {
-        throw new HypixelDiscordChatBridgeError("The linked data file does not exist. Please contact an administrator.");
-      }
+        const linkedData = readFileSync("data/linked.json");
+        if (!linkedData) {
+            throw new HypixelDiscordChatBridgeError("The linked data file does not exist. Please contact an administrator.");
+        }
 
-      const linked = JSON.parse(linkedData);
-      if (!linked) {
-        throw new HypixelDiscordChatBridgeError("The linked data file is malformed. Please contact an administrator.");
-      }
+        const linked = JSON.parse(linkedData);
+        if (!linked) {
+            throw new HypixelDiscordChatBridgeError("The linked data file is malformed. Please contact an administrator.");
+        }
 
-      if (user !== undefined) {
-        interaction.user = user;
-        interaction.member = await guild.members.fetch(interaction.user.id);
-      }
+        if (user !== undefined) {
+            interaction.user = user;
+            interaction.member = await guild.members.fetch(interaction.user.id);
+        }
 
-      if (!interaction.member) {
-        interaction.member = await guild.members.fetch(interaction.user.id);
-      }
+        if (!interaction.member) {
+            interaction.member = await guild.members.fetch(interaction.user.id);
+        }
 
-      const uuid = linked[interaction.user.id];
-      if (!uuid) {
-        throw new HypixelDiscordChatBridgeError("You are not linked to a Minecraft account.");
-      }
+        const uuid = linked[interaction.user.id];
+        if (!uuid) {
+            throw new HypixelDiscordChatBridgeError("You are not linked to a Minecraft account.");
+        }
 
-      const [hypixelGuild, player] = await Promise.all([
-        hypixelRebornAPI.getGuild("player", bot.username),
-        hypixelRebornAPI.getPlayer(uuid),
-      ]);
+        const [hypixelGuild, player] = await Promise.all([
+            hypixelRebornAPI.getGuild("player", bot.username),
+            hypixelRebornAPI.getPlayer(uuid),
+        ]);
 
-      if (!hypixelGuild) {
-        throw new HypixelDiscordChatBridgeError("Guild not found.");
-      }
+        if (!hypixelGuild) {
+            throw new HypixelDiscordChatBridgeError("Guild not found.");
+        }
 
-      interaction.member.setNickname(player.nickname, "Updated Roles");
+        interaction.member.setNickname(player.nickname, "Updated Roles");
 
-      const updateRole = new SuccessEmbed(
-        `<@${interaction.user.id}>'s roles have been successfully synced with \`${player.nickname}\`!`,
-        { text: `by @.kathund | /help [command] for more information`, iconURL: "https://i.imgur.com/uUuZx2E.png" }
-      );
+        const updateRole = new SuccessEmbed(
+            `<@${interaction.user.id}>'s roles have been successfully synced with \`${player.nickname}\`!`,
+            { text: `by @.kathund | /help [command] for more information`, iconURL: "https://i.imgur.com/uUuZx2E.png" }
+        );
 
-      if (interaction.replied || interaction.deferred) {
         await interaction.followUp({ embeds: [updateRole], ephemeral: true });
-      } else {
-        await interaction.reply({ embeds: [updateRole], ephemeral: true });
-      }
     } catch (error) {
-      const errorEmbed = new EmbedBuilder()
-        .setColor(15548997)
-        .setAuthor({ name: "An Error has occurred" })
-        .setDescription(`\`\`\`${error}\`\`\``)
-        .setFooter({
-          text: `by @.kathund | /help [command] for more information`,
-          iconURL: "https://i.imgur.com/uUuZx2E.png",
-        });
+        const errorEmbed = new EmbedBuilder()
+            .setColor(15548997)
+            .setAuthor({ name: "An Error has occurred" })
+            .setDescription(`\`\`\`${error}\`\`\``)
+            .setFooter({
+                text: `by @.kathund | /help [command] for more information`,
+                iconURL: "https://i.imgur.com/uUuZx2E.png",
+            });
 
-      if (interaction.replied || interaction.deferred) {
         await interaction.followUp({ embeds: [errorEmbed], ephemeral: true });
-      } else {
-        await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
-      }
     }
-  },
-};
+},
