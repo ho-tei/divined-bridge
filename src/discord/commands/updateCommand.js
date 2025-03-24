abc96165b8829d6ff8ef28cc2c9adc458ed13ea7
Exo <jorgeperezg24@hotmail.com>
2025-03-24 14:07:02 -0400

    Update updateCommand.js

--- src/discord/commands/updateCommand.js
+++ src/discord/commands/updateCommand.js
@@ -53,7 +53,7 @@ execute: async (interaction, user) => {
             { text: `by @.kathund | /help [command] for more information`, iconURL: "https://i.imgur.com/uUuZx2E.png" }
         );
 
-        await interaction.followUp({ embeds: [updateRole], ephemeral: true }); // Only follow up
+        await interaction.followUp({ embeds: [updateRole], ephemeral: true });
     } catch (error) {
         const errorEmbed = new EmbedBuilder()
             .setColor(15548997)
