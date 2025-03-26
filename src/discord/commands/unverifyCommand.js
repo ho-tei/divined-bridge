087483da3e9777c799ef6231745764f9685e6cec
Exo <jorgeperezg24@hotmail.com>
2025-03-26 09:57:49 -0400

    Update unverifyCommand.js

--- src/discord/commands/unverifyCommand.js
+++ src/discord/commands/unverifyCommand.js
@@ -11,8 +11,6 @@ module.exports = {
 
   execute: async (interaction) => {
     try {
-      await interaction.deferReply({ ephemeral: true});
-      
       const linkedData = readFileSync("data/linked.json");
       if (!linkedData) {
         throw new HypixelDiscordChatBridgeError(
@@ -27,24 +25,24 @@ module.exports = {
 
       const uuid = linked[interaction.user.id];
       if (uuid === undefined) {
-        throw new HypixelDiscordChatBridgeError(`You are not verified. Please run /verify to continue.`);
+        throw new HypixelDiscordChatBridgeError(You are not verified. Please run /verify to continue.);
       }
 
       delete linked[interaction.user.id];
       writeFileSync("data/linked.json", JSON.stringify(linked, null, 2));
 
       const updateRole = new SuccessEmbed(
-        `You have successfully unlinked \`${await getUsername(uuid)}\`. Run \`/verify\` to link a new account.`,
-        { text: `by @.kathund | /help [command] for more information`, iconURL: "https://i.imgur.com/uUuZx2E.png" },
+        You have successfully unlinked \${await getUsername(uuid)}\. Run \/verify\ to link a new account.,
+        { text: by @.kathund | /help [command] for more information, iconURL: "https://i.imgur.com/uUuZx2E.png" },
       );
-      await interaction.editReply({ embeds: [updateRole] });
+      await interaction.followUp({ embeds: [updateRole] });
     } catch (error) {
       const errorEmbed = new EmbedBuilder()
         .setColor(15548997)
         .setAuthor({ name: "An Error has occurred" })
-        .setDescription(`\`\`\`${error}\`\`\``)
+        .setDescription(\\\${error}\\\`)
         .setFooter({
-          text: `by @.kathund | /help [command] for more information`,
+          text: by @.kathund | /help [command] for more information,
           iconURL: "https://i.imgur.com/uUuZx2E.png",
         });
 
